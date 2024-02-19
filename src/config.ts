2051098760d7105d7b916f56c5891dfa97adc587
towardsuffering <christopher.rotunno@outlook.com>
2024-02-19 16:58:01 -0600

    first commit

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const SITE: Site = {
   website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
   author: "Sat Naing",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "ChrisBlogs",
+  title: "Chris Blogs",
   ogImage: "chrisblogsog.png",
   lightAndDarkMode: true,
   postPerPage: 3,
